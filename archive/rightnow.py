8121a883106f667bf4b89269251372085d761d98
pvdk <pumvandekoppel@gmail.com>
2025-10-06 03:02:38 +0200

    nieuwe versie van website (met mixtape)

---
+++ archive/rightnow.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+# accentgrave ~ now
+# HTML micro-now tool
+
+
+rightnowdir = "/home/pumvandekoppel/rightnow/"
+
+import os
+import sys
+import datetime
+from bs4 import BeautifulSoup
+from tidylib import tidy_document
+from mdx_linkify.mdx_linkify import LinkifyExtension
+import markdown
+
+
+
+def main(argv, verbose=False):
+  rightnow = argv[0]
+  # dot dot dot
+  #if not (rightnow.endswith('.') or rightnow.endswith('…') or rightnow.endswith('!') or rightnow.endswith('?')):
+  #  rightnow = rightnow + "."  
+
+  # transform passed markdown to HTML (wrapped in <p>), with linkify extension
+  # so that raw URLs are linkified
+  now_html = markdown.markdown(rightnow, extensions=[LinkifyExtension()])
+  
+  # read and update /now  
+  rightnow_doc = os.path.join(rightnowdir,"index.html")
+  with open(rightnow_doc, "r") as f:
+    # make soup
+    soup = BeautifulSoup(f, 'html5lib')
+    # find h1
+    h1 = soup.find("h1")
+    rightnow_soup = BeautifulSoup(now_html, 'html.parser')
+    
+    # what time is it?
+    nowtime = datetime.date.today()
+    time = soup.new_tag("time")    
+    time.string = nowtime.strftime("%d/%m/%Y").strip()
+
+    # find first p
+    p = rightnow_soup.find("p")
+    
+    # create a plain text version (for commit message)
+    txtrightnow = rightnow_soup.get_text()
+    
+    # insert content
+    p.insert(0,time)
+    h1.insert_after(rightnow_soup)
+
+    # clean HTML with Tidy
+    prettified = soup.prettify(formatter="minimal")
+    document, errors = tidy_document(prettified)
+    
+  # write /rightnow/index.html
+  with open(rightnow_doc, "w", encoding="utf-8", errors="xmlcharrefreplace") as output_file:      
+    output_file.write(document)
+  
+  # return cleaned text string to stdout
+  print(txtrightnow)
+  sys.exit()
+
+
+#if __name__ == "__main__":
+main(sys.argv[1:])
+
---
+++ archive/rightnow.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "Hey."
+echo "What are you listening to right now ? "
+read now
+
+cd ~/rightnow/
+# source ~rightnow/env/activate
+rightnow=`python ~/rightnow/rightnow.py "$now"`
+
+message="Right now: $rightnow"
+
+git add index.html
+git commit -m "${message}"
+git push
+
+echo "OK."
